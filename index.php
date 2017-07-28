be9f408141d65c69e9beb57371535e9feeaaa80e
Dartegnian Velarde <Dartegnian@users.noreply.github.com>
2017-07-29 00:59:19 +0800

    Dark switch

--- index.php
+++ index.php
@@ -28,17 +28,38 @@
 	</head>
 	<body>
 		<header>
-			<h1 style="display: inline;"> <img id="logo" src="assets\images\logoworkmark.png"/></h1>
+			<h1 style="display: inline;"> <img id="logo" src="assets\images\logowhite.png"/></h1>
+			<button style="display: inline-block;float: right; margin-left: -5px; margin-top: 10px;" class="userbutt" onclick="Redirect()">Register</button>
+			<form action="signingin.php" method="post" style="display: inline;">
+			<table border="0" style="display: inline; float: right;">
+				<tr style="display: inline;">
+				<td style="color: #777">Username</td>
+				<td align="left"><input class="inputfield" type="text" name="username" size="20" maxlength="50" /></td>
+				</tr>
+				<tr style="display: inline;">
+				<td style="color: #777">Password</td>
+				<td align="left"><input class="inputfield" type="password" name="password" size="20" maxlength="50" /></td>
+				</tr>
+				<tr style="display: inline;">
+				<td style="border-right: 1px solid #e4e4e4; padding-right: 10px;" colspan="2" align="center"><input type="submit" value="Log In" class="button"/></td>
+				</tr>
+				<tr style="display: inline;">
+				<td colspan="2" align="center">
+				</td>
+				</tr>
+			</table>
+		</form>
 			<nav>
 				<a href="link1.html" id="current">Home</a>
-				<a href="link2.html">Management</a>
-				<a href="link2.html">Management</a>
-				<a href="link2.html">Management</a>
-				<a href="link2.html">Management</a>
+				<a href="link2.html">Dashboard</a>
+				<a href="link2.html">Mobile App</a>
+				<a href="link2.html">About</a>
 			</nav>
 		</header>
+		<section id="filler">
+		</section>
 		<section id="hero">
-			<h1 class="header"><span class="rotate">CYNTHIA, THEME, WEW</span></h1>
+			<h1 class="header"><span class="rotate">A NEW WAY FOR EMPLOYEES TO EXPERIENCE WORK, A FASTER WAY, A BETTER WAY, WORKAHOLIC LOGIN SUITE</span></h1>
 		</section>
 		<footer>
 			<div id="backtotop">
