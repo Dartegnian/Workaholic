1c4bdb66a78584b1959a9fb04f7c13d04741c477
Dartegnian Velarde <Dartegnian@users.noreply.github.com>
2017-07-29 14:41:41 +0800

    Before XAMPP

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
 		<title>Workaholic Suite</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/dashboard.css" rel="stylesheet">
 		<link rel="icon" type="image/png" href="assets/images/logosmallsquare.png">
 		<script src='assets/js/jquery.min.js'></script>
 		<script src='assets/js/textrotator.js'></script>
@@ -32,20 +32,14 @@
 	</head>
 	<body>
 		<header>
-			<h1 style="display: inline;"> <img id="logo" src="assets\images\logowhite.png"/></h1>
-			<button style="display: inline-block;float: right; margin-left: -5px; margin-top: 4px; margin-right: 30px;" class="userbutt" onclick="Redirect()">Register</button>
+			<h1 style="display: inline;"> <a href="index.php"><img id="logo" src="assets\images\logowhite.png"/></a></h1>
 			<form action="dashboard.php" method="post" style="display: inline;">
 				<table border="0" style="display: inline; float: right;">
 					<tr style="display: inline;">
-					<td style="color: #fff">Username</td>
-					<td align="left"><input class="inputfield" type="text" name="username" size="20" maxlength="50" style="background: url(assets/images/people.png); background-color: rgba(100, 100, 100, 0.3); background-size: 30px; padding-left: 35px; background-repeat: no-repeat;"></td>
+					<td align="left"><input class="inputfield" type="text" readonly="yes" name="username" value="dartegnian" size="20" maxlength="50" style="background: url(assets/images/people.png); background-color: rgba(100, 100, 100, 0.3); background-size: 30px; padding-left: 35px; background-repeat: no-repeat;"></td>
 					</tr>
 					<tr style="display: inline;">
-					<td style="color: #fff">Password</td>
-					<td align="left"><input class="inputfield" type="password" name="password" style="background: url(assets/images/lock.png); background-color: rgba(100, 100, 100, 0.3); background-size: 30px; padding-left: 35px; background-repeat: no-repeat;" size="20" maxlength="50" /></td>
-					</tr>
-					<tr style="display: inline;">
-					<td style="border-right: 1px solid #e4e4e4; padding-right: 10px;" colspan="2" align="center"><input type="submit" value="Log In" class="button"/></td>
+					<td style="border-right: 0px solid #e4e4e4; padding-right: 10px;" colspan="2" align="center"><input type="submit" value="Log Out" class="button"/></td>
 					</tr>
 					<tr style="display: inline;">
 					<td colspan="2" align="center">
@@ -54,11 +48,10 @@
 				</table>
 			</form>
 			<nav>
-				<a id="current">Home</a>
-				<a onclick="popup()">Dashboard</a>
-				<a href="link2.html">Mobile App</a>
-				<a href="link2.html">Browser Extension</a>
-				<a href="link2.html">About</a>
+				<a id="current" style="border-right: 1px solid #e4e4e4;">Dashboard</a>
+				<a id="current" href="link2.html" style="padding-left: 25px;">Summary</a>
+				<a href="link2.html">Leaderboard</a>
+				<a href="link2.html">Available Prizes</a>
 			</nav>
 		</header>
 		<section id="filler">
